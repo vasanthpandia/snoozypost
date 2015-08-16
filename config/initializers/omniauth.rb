4f4f8dc0387df567271613a58b8a4b0ec38904b1
Vasanth <vasanth@Vasanths-MacBook-Pro.local>
2015-08-17 00:06:36 +0530

    env variables fixed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,8 @@ require 'yaml'
 
 module Twitter
   # CONFIG = YAML.load_file(Rails.root.join("config/twitter.yml"))[Rails.env]
-  API_KEY = ENV['TWITTER_API_KEY']
-  SECRET = ENV['TWITTER_SECRET']
+  API_KEY = ENV['TWITTER_APP_KEY']
+  SECRET = ENV['TWITTER_APP_SECRET']
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
