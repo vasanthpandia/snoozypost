b63619285b8671c9a18e7b162a2d95c6b7f6718c
Vasanth <vasanth@Vasanths-MacBook-Pro.local>
2015-08-17 00:13:26 +0530

    changed in twitter.rb

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
-TWITTER_KEY = "JI6S08yaB0NlaKkWjeV3KAejm"
-	TWITTER_SECRET = ENV['TWITTER_SECRET']
+TWITTER_KEY = ENV['TWITTER_APP_KEY']
+	TWITTER_SECRET = ENV['TWITTER_APP_SECRET']
 	oauthconsumer = OAuth::Consumer.new TWITTER_KEY, TWITTER_SECRET, {:site => "https://api.twitter.com"}
 	TWITTER_AUTH = oauthconsumer.get_request_token.authorize_url
