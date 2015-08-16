e5e8a61667ce7a370e294fa26c2a60c8d12a69be
Vasanth <vasanth@Vasanths-MacBook-Pro.local>
2015-08-17 00:00:41 +0530

    Koala changed

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -3,9 +3,9 @@
 # automatically use Facebook settings from here if none are given
 require 'yaml'
 module Facebook
-  CONFIG = YAML.load_file(Rails.root.join("config/facebook.yml"))[Rails.env]
-  APP_ID = CONFIG['app_id']
-  SECRET = CONFIG['secret_key']
+  # CONFIG = YAML.load_file(Rails.root.join("config/facebook.yml"))[Rails.env]
+  APP_ID = ENV['FACEBOOK_APP_ID']
+  SECRET = ENV['FACEBOOK_APP_SECRET']
 end
 
 Koala::Facebook::OAuth.class_eval do
