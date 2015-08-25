6ad43af7efa6061fa6f4372096110695c45c92c2
Vasanth <vasanth@Vasanths-MacBook-Pro.local>
2015-08-25 18:57:23 +0530

    initializers readded

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,4 @@
+TWITTER_KEY = ENV['TWITTER_APP_KEY']
+TWITTER_SECRET = ENV['TWITTER_APP_SECRET']
+oauthconsumer = OAuth::Consumer.new TWITTER_KEY, TWITTER_SECRET, {:site => "https://api.twitter.com"}
+TWITTER_AUTH = oauthconsumer.get_request_token.authorize_url
